32577545e32044c955506685fdedbaf1eb9941b3
ash-rick <ashrickgupta02@gmail.com>
2024-10-21 13:13:12 +0530

    Port restricted to 3000 and title added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,16 @@ import type { AppProps } from "next/app";
 import "react-toastify/dist/ReactToastify.css";
 
 import { ToastContainer } from "react-toastify";
+import Head from "next/head";
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>New Task Dashboard</title>
+        <meta name="description" content="Manage your tasks effectively." />
+      </Head>
       <Component {...pageProps} />
       <ToastContainer />
     </>
